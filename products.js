8e2df05e5732c62d8871bf06f9279d5d504e9e32
Ewerton Silva <ewerton1284@gmail.com>
2024-11-27 11:59:05 -0300

    Atualização

--- products.js
+++ products.js
@@ -29,7 +29,7 @@ closeCart.addEventListener('click', () => {
                 `<img src="${product.image}" alt="">
                 <h2>${product.name}</h2>
                 <div class="price">$${product.price}</div>
-                <button class="addCart">Add To Cart</button>`;
+                <button class="addCart">Adicionar ao carrinho</button>`;
                 listProductHTML.appendChild(newProduct);
             });
         }
